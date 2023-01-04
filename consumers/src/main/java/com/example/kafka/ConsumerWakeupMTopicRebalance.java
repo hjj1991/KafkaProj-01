9ea79d895261609ae89eca0e55946dfcb8b6f101
hjj1991 <hjj19911@naver.com>
2023-01-05 00:04:44 +0900

    Consumer 학습중

---
+++ consumers/src/main/java/com/example/kafka/ConsumerWakeupMTopicRebalance.java
@@ -0,0 +1,69 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerWakeupMTopicRebalance {
+
+    public static final Logger logger = LoggerFactory.getLogger(ConsumerWakeupMTopicRebalance.class);
+
+    public static void main(String[] args) {
+
+//        String topicName = "pizza-topic";
+ 
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.28.200:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+//        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-01");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-assign");
+//        props.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "3");
+//        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName());
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
+        kafkaConsumer.subscribe(List.of("topic-p3-t1", "topic-p3-t2"));
+
+        //main thread 참조 변수
+        Thread mainThread = Thread.currentThread();
+
+        //main thread 종료시 별도의 thread로 kafkaconsumer wakeup()메소드를 호출하게 함.
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                logger.info(" main program starts to exit by calling wakeup");
+                kafkaConsumer.wakeup();
+                try{
+                    mainThread.join();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<String, String> record : consumerRecords) {
+                    logger.info("topic:{}, record key:{}, partition: {}, record offset:{},  record value:{}",
+                            record.topic(), record.key(), record.partition(), record.offset(),  record.value());
+                }
+            }
+        }catch (WakeupException e) {
+            logger.error("wakeup exception has been called");
+        }finally {
+            logger.info("finally consumer is closing");
+            kafkaConsumer.close();
+        }
+
+
+
+//        kafkaConsumer.close();
+    }
+}
